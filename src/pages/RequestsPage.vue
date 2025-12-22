c6cf38bbb58adaab846c3b373127acab3df35685
Ilia Baev <baevilia.201@yandex.ru>
2025-12-23 00:51:42 +0300

    Добавил запрос на получение заявок

--- src/pages/RequestsPage.vue
+++ src/pages/RequestsPage.vue
@@ -3,6 +3,28 @@
 </template>
 
 <script setup lang="ts">
+import requests from "../services/requests.ts";
+import {onMounted, ref} from "vue";
+interface Request{
+  id: number;
+  title: string;
+  description: string;
+}
+
+const requestsList = ref<Request[]>([]);
+
+const fetchRequests = async () => {
+  try{
+    const response = await requests.getRequests();
+
+    requestsList.value = response.data.data;
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+onMounted(fetchRequests())
 
 </script>
 
---
+++ src/services/requests.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+    baseURL: "http://localhost:5122",
+    headers:{
+        "Content-Type": "application/json",
+    }
+})
+
+export default {
+    getRequests() {
+        return apiClient.get('/requests');
+    }
+}
